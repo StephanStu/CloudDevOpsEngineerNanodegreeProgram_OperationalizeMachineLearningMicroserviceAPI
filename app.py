6d00dbb9a44d8616ba7e8c57566a9d7c33f0795b
Stephan Studener <stephanstudener@Stephans-MBP.fritz.box>
2020-05-21 19:35:12 +0200

    feat: files adopeted from gitubc.om/noahgift/

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-def myfunc():
-    return 1
+from flask import Flask
+app = Flask(__name__)
 
-print(myfunc())
+@app.route('/')
+def hello_world():
+    return 'Hello, World from Stephan!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
