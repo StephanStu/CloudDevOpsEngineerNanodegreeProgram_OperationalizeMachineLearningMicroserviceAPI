fc9acc764e5ecd5338c9558e554a8b61c9d7d494
Stephan Studener <62570946+StephanStu@users.noreply.github.com>
2020-05-23 15:56:37 +0200

    fix: change a comment to trigger circleci

--- app.py
+++ app.py
@@ -67,6 +67,6 @@ def predict():
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
-    # load pretrained model as clf
+    # load pretrained model as clf.
     clf = joblib.load("./model_data/boston_housing_prediction.joblib")
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
